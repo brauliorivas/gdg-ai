f3fa2fcff4b2e7797a4986475bc33fc0dd5865ab
Erick <127678881+Erillope@users.noreply.github.com>
2024-05-02 19:03:03 -0500

    Quité las claves del braulio xd

--- src/load.py
+++ src/load.py
@@ -2,10 +2,10 @@ from src.repositories.repository_builder import QdrantRepositoryBuilder
 
 
 builder = QdrantRepositoryBuilder(
-    gemini_api_key="AIzaSyDAxCHSpGTUyuTHzbu3IwM8OCwSn0N4jRk",
-    qdrant_url="https://ffbaa1f9-05e7-4f92-85e3-cd44bf8f274a.us-east4-0.gcp.cloud.qdrant.io:6333",
-    qdrant_api_key="nAgRIItF-ypjDc3kHBw3-az9_yfi8wSFPZmQ5-Ro2W3--76jWPkx4g",
-    collection_name='products2'
+    gemini_api_key="GEMINI KEY",
+    qdrant_url="URL",
+    qdrant_api_key="QDRANT KEY",
+    collection_name='name'
 )
 
 repository = builder.build()
