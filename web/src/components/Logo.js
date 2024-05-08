e2c159e61ed53fae7b39087d7ecc81464f3a8d4f
hombr <kdfernan@espol.edu.ec>
2024-05-07 20:56:05 -0500

    Init Front-end

---
+++ web/src/components/Logo.js
@@ -0,0 +1,45 @@
+export function Logo(){
+    return(
+        <div className="flex border border-sky-500 p-2">
+            <table>
+                <tbody>
+                <tr>
+                    <td className="pt-1 pl-1">
+                        <div className="h-3 w-3 bg-orange-600 "></div>
+                    </td>
+                    <td className="pt-1 pl-1">
+                        <div className="h-3 w-3 bg-orange-600"></div>
+                    </td>
+                    <td className="pt-1 pl-1 pr-1 transform rotate-45">
+                        <div className="h-3 w-3 bg-orange-600"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="pt-1 pl-1">
+                        <div className="h-3 w-3 bg-orange-600 "></div>
+                    </td>
+                    <td className="pt-1 pl-1">
+                        <div className="h-3 w-3 bg-orange-600"></div>
+                    </td>
+                    <td className="pt-1 pl-1 pr-1">
+                        <div className="h-3 w-3 bg-orange-600"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="pt-1 pl-1">
+                        <div className="h-3 w-3 bg-orange-600 "></div>
+                    </td>
+                    <td className="pt-1 pl-1">
+                        <div className="h-3 w-3 bg-orange-600"></div>
+                    </td>
+                    <td className="pt-1 pl-1 pr-1">
+                        <div className="h-3 w-3 bg-orange-600"></div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <div className="h-9 bg-slate-500 w-1" > </div>
+            <h1> El Puente</h1>
+        </div>
+    );
+}
