6ae354b3f6f5ccda68a9ec057daa5f0891960ae4
hombr <kdfernan@espol.edu.ec>
2024-05-10 15:42:22 -0500

    Chat 1.3

--- web/src/components/Message.jsx
+++ web/src/components/Message.jsx
@@ -1,28 +1,23 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default function Message({ text, link = "https://github.com/shadcn.png", orientation}) {
+export default function Message({ text, link = "https://github.com/shadcn.png", orientation }) {
   return (
-    <div className="flex flex-row items-center">
-      {orientation === "left" && (
-        <div>
+    <div className={`flex ${orientation === "right" ? "justify-end" : "justify-start"}`}>
+      <div className={`max-w-half flex items-center m-3 ${orientation === "right" ? "bg-blue-500" : "bg-green-500"} text-black p-2 rounded-lg shadow-md`}>
+        {orientation === "left" && (
           <Avatar>
             <AvatarImage src={link} />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-        </div>
-      )}
-      <div>
-        <div>{text}</div>
+        )}
+        <div className="mx-2">{text}</div>
+        {orientation === "right" && (
+          <Avatar>
+            <AvatarImage src={link} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        )}
       </div>
-      {orientation === "right" && (
-        <div>
-          <Avatar>
-            <AvatarImage src={link} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
-      )}
     </div>
-  )
+  );
 }
-
